1932a16413d5a64dec4af695db8c60cf4754d796
JacobHsu <jacob.hsu.tw@gmail.com>
2016-05-09 09:48:04 +0800

    fix error test case

--- test/test.js
+++ test/test.js
@@ -4,7 +4,6 @@ describe('Array', function(){
     it('should return -1 when the value is not present', function(){
       //assert.equal(1, [1,2,3].indexOf(0)); //failing
       assert.equal(-1, [1,2,3].indexOf(0)); //passing
-      assert.equal(3, [1,2,3].indexOf(5)); //failing
     })
   })
 })
